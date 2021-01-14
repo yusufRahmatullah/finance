2cfb86a1e1d380a1c927dee3d41993d1dac4adbf
Yusuf Rahmatullah <yusufr.main@gmail.com>
2021-01-14 19:50:56 +0700

    add budget filter on transaction

--- static/js/transaction.js
+++ static/js/transaction.js
@@ -1,7 +1,14 @@
-function generateTable(trxs, totalBudget) {
+var transactionData = [];
+var totalBudget = 0;
+
+function generateTable() {
   var tableNode = q('#table-body');
   var ctn = '';
   var currentBudget = totalBudget;
+  var form = document.forms['form'];
+  var budget = q('#budget-name-options').value;
+
+  tableNode.innerHTML = '';
 
   ctn += transactionView({
     date: '',
@@ -11,32 +18,70 @@ function generateTable(trxs, totalBudget) {
     income: totalBudget,
   }, 0, true);
 
-  trxs.forEach(trx => {
-    ctn += transactionView(trx, currentBudget);
+  transactionData.forEach(trx => {
+    if (budget == 'all' || budget == trx.budget) {
+      ctn += transactionView(trx, currentBudget);
+    }
     currentBudget = currentBudget + trx.income - trx.outcome;
   });
   appendNode(tableNode, ctn);
 }
 
-function getTransactions(totalBudget) {
+function getTransactions() {
   setPeriod();
 
   fetch('/transactions/get')
   .then(resp => resp.json())
   .then(data => {
-    generateTable(data, totalBudget);
+    transactionData = data;
+    generateTable();
   })
   .catch(err => {
     console.error("Failed get transactions. " + err);
   })
 }
 
+function initBudgetNames(names) {
+  var node = q('#budget-name-options');
+  var ctn = '';
+
+  names.forEach(name => {
+    var norm_name = humanizeName(name);
+    ctn += `<option value="${name}">${norm_name}</option>`;
+  });
+
+  appendNode(node, ctn);
+}
+
+function initBudgets() {
+  // get budget names first
+  fetch('/budgets/names')
+  .then(resp => resp.json())
+  .then(data => {
+    initBudgetNames(data);
+    initMaterials();
+  })
+  .catch(err => {
+    console.error("Failed load transactions. " + err);
+  })
+}
+
+function initMaterials() {
+  // select
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+}
+
 function loadTransactions() {
+  // to init budget name select
+  initBudgets();
+
   // get total budgets first
   fetch('/budgets/total')
   .then(resp => resp.json())
   .then(data => {
-    getTransactions(data.total);
+    totalBudget = data.total;
+    getTransactions();
   })
   .catch(err => {
     console.error("Failed load transactions. " + err);
